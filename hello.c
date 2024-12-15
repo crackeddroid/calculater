475f8d440b7006fcac6fb3b89ec50d5ff59ba068
crackeddroid <arshadansai503@gmail.com>
2024-12-15 07:59:24 +0530

    New Upadte

--- hello.c
+++ hello.c
@@ -1,5 +1,6 @@
-#include<stdio.h>
-void menu(){
+#include <stdio.h>
+void menu()
+{
     printf("\n********Calculater**********\n");
     printf("\n1: Add");
     printf("\n2: Substract");
@@ -10,19 +11,47 @@ void menu(){
     printf("\n7:Exit\n");
     printf("Enter your choice (1 to 7) : ");
 }
-int main(){
-    int choice,a,b,result;
+int main()
+{
+    float result;
+    int a, b,choice;
     while (1)
     {
-    menu();
-    scanf("%d",&choice);
-    if(choice == 7){
-        break;
+        menu();
+        scanf("%d", &choice);
+        if (choice == 7)
+        {
+            break;
+        }
+        printf("Enter first number : ");
+        scanf("%d", &a);
+        printf("Enter secound number : ");
+        scanf("%d", &b);
+        switch (choice)
+        {
+        case 1:
+            result = a + b;
+            printf("Your Result is %.2f\n\n", result);
+            break;
+        case 2:
+            result = a - b;
+            printf("Your Result is %.2f\n\n", result);
+            break;
+        case 3:
+            result = a * b;
+            printf("Your Result is %.2f\n\n", result);
+            break;
+        case 4:
+            result = a / b;
+            printf("Your Result is %.2f\n\n", result);
+            break;
+        case 5:
+            result = a % b;
+            printf("Your Result is %d\n\n", result);// modified
+            break;
+        default:
+            printf("Please enter corret input");
+            break;
+        }
     }
-    printf("Enter first number : ");
-    scanf("%d",&a);
-    printf("Enter secound number : ");
-    scanf("%d",&b);
-    } 
-    
 }
